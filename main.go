c4f199980fed14222e03ef2ed672a728dcc8d132
周黎 <zl6216@ly.com>
2018-04-04 15:51:32 +0800

    增加中文

--- main.go
+++ main.go
@@ -16,9 +16,9 @@ const (
 
 type server struct{}
 
-// SayHello implements helloworld.GreeterServer 
+// SayHello implements helloworld.GreeterServer
 func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
-	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
+	return &pb.HelloReply{Message: "hi，你好 " + in.Name}, nil
 }
 func main() {
 	lis, err := net.Listen("tcp", port)
